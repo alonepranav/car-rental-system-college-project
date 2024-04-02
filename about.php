8c537312181459298a741f9a4dac9db6f516e47e
= <=>
2024-04-02 10:11:54 +0530

    New

--- about.php
+++ about.php
@@ -6,15 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/output.css">
+    <link rel="stylesheet" href="./css/output.css"> 
 </head>
 
 <body>
 
-    <?php include("./components/Navbar.html"); ?>
-
-
-
+    <?php include("./components/Navbar.html"); ?> 
 
     <div class='px-40 mt-32'>
 
